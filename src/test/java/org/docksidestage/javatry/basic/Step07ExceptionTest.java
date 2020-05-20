417296cfd1be4b2dc12b984f15277e4540235f79
hayato-sasaki <hayato.sasaki@bizreach.co.jp>
2020-05-20 11:03:15 +0900

    [Step07] answer test_exception_cause_basic

--- src/test/java/org/docksidestage/javatry/basic/Step07ExceptionTest.java
+++ src/test/java/org/docksidestage/javatry/basic/Step07ExceptionTest.java
@@ -128,9 +128,9 @@ public class Step07ExceptionTest extends PlainTestCase {
             Throwable cause = e.getCause();
             sea = cause.getMessage();
             land = cause.getClass().getSimpleName();
-            log(sea); // your answer? => 
-            log(land); // your answer? => 
-            log(e); // your answer? => 
+            log(sea); // your answer? => Failed to call the third help method: -1
+            log(land); // your answer? => IllegalArgumentException
+            log(e); // your answer? => NumberFormatException
         }
     }
 
