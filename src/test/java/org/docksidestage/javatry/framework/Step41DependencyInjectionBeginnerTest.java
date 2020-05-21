73db55d708d13813719eb26ee3101f94ea331efe
hayato-sasaki <hayato.sasaki@bizreach.co.jp>
2020-05-21 17:13:13 +0900

    [Step41] answer test_zone_search_commponent_on_LastDi

--- src/test/java/org/docksidestage/javatry/framework/Step41DependencyInjectionBeginnerTest.java
+++ src/test/java/org/docksidestage/javatry/framework/Step41DependencyInjectionBeginnerTest.java
@@ -283,7 +283,8 @@ public class Step41DependencyInjectionBeginnerTest extends PlainTestCase {
      * https://github.com/lastaflute/lastaflute-example-harbor
      */
     public void test_zone_search_component_on_LastaDi() {
-        // your answer? => 
+        // your answer? => src/main/resources/app.xml および src/main/resources/dbflute.xml
+        // NOTE http://dbflute.seasar.org/ja/lastaflute/lastadi/index.html#richcomp
     }
 
     /**
